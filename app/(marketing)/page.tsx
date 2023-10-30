8d3b315ddaf91b69a2dbcd8175556b2a2274fcfd
DavidGots <gotsiridzedato@yahoo.com>
2023-10-30 13:52:05 +0400

    page color error

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import { Heroes } from "./_components/heroes";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col text-center items-center justify-center gap-y-8 flex-1 px-6 pb-10 md:justify-start">
         <Heading />
         <Heroes />
